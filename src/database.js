7fcb74dfb1354d18ec48745eec06bab805ef3662
Elmira Ibragimova <elmira.ibragimova28@gmail.com>
2019-12-23 10:50:15 +0300

    set database

---
+++ src/database.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDQYdtzxoJb0pd6-x839U8GPb-Qv8plSow',
+  authDomain: 'chat-app-b936f.firebaseapp.com',
+  databaseURL: 'https://chat-app-b936f.firebaseio.com',
+  projectId: 'chat-app-b936f',
+  storageBucket: 'chat-app-b936f.appspot.com',
+  messagingSenderId: '800881023086'
+}
+
+firebase.initializeApp(firebaseConfig)

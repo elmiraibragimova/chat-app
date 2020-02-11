93ba26c8216daf739de9858d0fc52fa57c3cac32
Elmira Ibragimova <elmira.ibragimova28@gmail.com>
2020-02-11 23:41:51 +0300

    rewrite header component using hooks

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,57 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
+import { useSelector } from 'react-redux'
+
 import { Link } from 'react-router-dom'
 import LogoutDialog from '../LogoutDialog/LogoutDialog'
 import './Header.scss'
-import { connect } from 'react-redux'
 
-const mapStoreToProps = state => {
-  return {
-    currentUser: state.currentUser
-  }
+const Header = ({ className, history, notify }) => {
+  const [isLogoutDialogOpen, toggleLogoutDialog] = useState(false)
+
+  const currentUser = useSelector(state => state.currentUser)
+  const { name, photoUrl } = currentUser || {}
+
+  return (
+    <>
+      <header className={`${className} header`}>
+        <Link className={'link-to-profile'} to="/profile">
+          <div className="header__user">
+            <img className="header__avatar" src={photoUrl} alt="Avatar" />
+            <span className="header__user-name">{name}</span>
+          </div>
+        </Link>
+        <button
+          className="logout-button"
+          onClick={() => toggleLogoutDialog(true)}
+        >
+          Log out
+        </button>
+      </header>
+
+      {!!isLogoutDialogOpen && (
+        <LogoutDialog
+          cancelLogoutDialog={() => toggleLogoutDialog(false)}
+          history={history}
+          notify={notify}
+        />
+      )}
+    </>
+  )
 }
 
-class Header extends Component {
-  state = {
-    isLogoutDialogOpen: false
-  }
-
-  openLogoutDialog = async () => {
-    this.setState({ isLogoutDialogOpen: true })
-  }
-
-  cancelLogoutDialog = () => {
-    this.setState({ isLogoutDialogOpen: false })
-  }
-
-  render() {
-    const { name, photoUrl } = this.props.currentUser || {}
-
-    return (
-      <>
-        <header className={`${this.props.className} header`}>
-          <Link className={'link-to-profile'} to="/profile">
-            <div className="header__user">
-              <img className="header__avatar" src={photoUrl} alt="Avatar" />
-              <span className="header__user-name">{name}</span>
-            </div>
-          </Link>
-          <button className="logout-button" onClick={this.openLogoutDialog}>
-            Log out
-          </button>
-        </header>
-
-        {!!this.state.isLogoutDialogOpen && (
-          <LogoutDialog
-            cancelLogoutDialog={this.cancelLogoutDialog}
-            history={this.props.history}
-            notify={this.props.notify}
-          />
-        )}
-      </>
-    )
-  }
-}
-
-const ConnectedHeader = connect(mapStoreToProps)(Header)
-
-export default ConnectedHeader
+export default Header

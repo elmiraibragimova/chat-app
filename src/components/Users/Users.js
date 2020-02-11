2494fa87d55d2891aa9ec37d01873f7069b1ddb4
Elmira Ibragimova <elmira.ibragimova28@gmail.com>
2020-02-12 00:03:21 +0300

    rewrite users using hooks

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,57 +1,46 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React from 'react'
+import { useSelector } from 'react-redux'
 import { ReactComponent as Bookmark } from '../Board/images/bookmark.svg'
 import './Users.scss'
 
-const mapStateToProps = state => {
-  return { users: state.users }
-}
+const Users = props => {
+  const users = useSelector(state => state.users)
 
-class Users extends Component {
-  componentDidMount() {}
-
-  render() {
-    return (
-      <ul className="users">
-        {this.props.users.map((it, index) => {
-          return (
-            <li
-              className={
-                this.props.currentPeerUser &&
-                this.props.currentPeerUser.id === it.id
-                  ? 'user user_active'
-                  : 'user'
-              }
-              key={index}
-              onClick={() => this.props.selectPeerUser(it)}
-            >
-              {this.props.currentUserId === it.id ? (
-                <div className="user__bookmark-container">
-                  <Bookmark className="user__bookmark" />
-                </div>
-              ) : (
-                <img
-                  className="user__avatar"
-                  src={it.photoUrl}
-                  alt={`${it.name} avatar`}
-                />
-              )}
-
-              <div className="user__info">
-                <span className="user__name">
-                  {this.props.currentUserId === it.id
-                    ? 'Saved messages'
-                    : it.name}
-                </span>
+  return (
+    <ul className="users">
+      {users.map((it, index) => {
+        return (
+          <li
+            className={
+              props.currentPeerUser && props.currentPeerUser.id === it.id
+                ? 'user user_active'
+                : 'user'
+            }
+            key={index}
+            onClick={() => props.selectPeerUser(it)}
+          >
+            {props.currentUserId === it.id ? (
+              <div className="user__bookmark-container">
+                <Bookmark className="user__bookmark" />
               </div>
-            </li>
-          )
-        })}
-      </ul>
-    )
-  }
+            ) : (
+              <img
+                className="user__avatar"
+                src={it.photoUrl}
+                alt={`${it.name} avatar`}
+              />
+            )}
+
+            <div className="user__info">
+              <span className="user__name">
+                {props.currentUserId === it.id ? 'Saved messages' : it.name}
+              </span>
+            </div>
+          </li>
+        )
+      })}
+    </ul>
+  )
 }
 
-const ConnectedUsers = connect(mapStateToProps)(Users)
-
-export default ConnectedUsers
+export default Users
